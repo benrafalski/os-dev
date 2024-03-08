c2f0c1bb48cca77249062907e658b4fba48a42da
benjamin.rafalski@intel.com <brafalsk@asu.edu>
2024-03-08 09:43:15 -0700

    loaded sectors from the disk

--- boot_sect.asm
+++ boot_sect.asm
@@ -1,24 +1,15 @@
-;
-; A boot sector that prints a string using our function.
-;
+
 [org 0x7c00] ; Tell the assembler where this code will be loaded
-; mov bx, HELLO_MSG ; Use BX as a parameter to our function , so
-; call print_string ; we can specify the address of a string.
-; mov bx, GOODBYE_MSG
-; call print_string
 
-
-mov bx, [HEX_WORD]
-call print_hex
 
 jmp $ ; Hang
 
 %include "print_string.asm"
 %include "print_hex.asm"
+%include "disk_load.asm"
+
 ; Data
-HELLO_MSG: db 'Hello, World!', 0
-GOODBYE_MSG: db 'Goodbye!', 0
-HEX_WORD: dw 0xdead
+
 ; Padding and magic number.
 times 510-($-$$) db 0
 dw 0xaa55
