ed8a62d7740a325b614c45a94def4b786f8b36d1
benjamin.rafalski@intel.com <brafalsk@asu.edu>
2024-03-08 09:09:09 -0700

    print hex

---
+++ print_hex.asm
@@ -0,0 +1,67 @@
+print_hex:
+    ; store the hex value in bx
+    ; print '0x'
+    mov ah, 0x0e    
+    mov al , '0'
+    int 0x10
+    mov ah, 0x0e    
+    mov al , 'x'
+    int 0x10
+
+print_byte_1:
+    mov cx, bx
+    ; print lower byte
+    mov bl, bh
+    and bh, 0xf0     ; bh = 0x10
+    shr bh, 0x4     ; bh = 0x01
+    and bl, 0x0f    ; bl = 0x09
+    add bh, 0x30
+    add bl, 0x30
+
+    cmp bh, 0x39
+    jle checkbl_1
+    add bh, 0x7
+    
+checkbl_1:
+
+    cmp bl, 0x39
+    jle clean_1
+    add bl, 0x7
+
+clean_1:
+
+    mov ah, 0x0e
+    mov al, bh 
+    int 0x10
+    mov ah, 0x0e
+    mov al, bl 
+    int 0x10
+
+print_byte_2:
+    ; print higher byte
+    mov bx, cx
+    mov bh, bl
+    and bh, 0xf0     ; bh = 0x10
+    shr bh, 0x4     ; bh = 0x01
+    and bl, 0x0f    ; bl = 0x09
+    add bh, 0x30
+    add bl, 0x30
+
+    cmp bh, 0x39
+    jle checkbl_2
+    add bh, 0x7
+    
+checkbl_2:
+    cmp bl, 0x39
+    jle clean_2
+    add bl, 0x7
+
+clean_2:
+    mov ah, 0x0e
+    mov al, bh 
+    int 0x10
+    mov ah, 0x0e
+    mov al, bl 
+    int 0x10
+    ret
+
