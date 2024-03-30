96487c2fa1ca8ce08ff1e2b37f5084c62757ca6d
benrafalski <79751177+benrafalski@users.noreply.github.com>
2024-03-29 18:51:49 -0700

    trying to print which exception is happening

--- BennyOS/kernel/interrupts/isr.asm
+++ BennyOS/kernel/interrupts/isr.asm
@@ -1,11 +1,13 @@
 %macro isr_err_stub 1
 isr_stub_%+%1:
+    mov rax, 0x41
     call exception_handler
     iretq 
 %endmacro
 ; if writing for 64-bit, use iretq instead
 %macro isr_no_err_stub 1
 isr_stub_%+%1:
+    mov rax, 0x42
     call exception_handler
     iretq
 %endmacro
--- BennyOS/kernel/interrupts/isr.h
+++ BennyOS/kernel/interrupts/isr.h
@@ -3,6 +3,22 @@
 __attribute__((noreturn)) void exception_handler(void);
 void exception_handler()
 {
-    __asm__ volatile("cli; hlt"); // Completely hangs the computer
+    int src = 1;
+    int dst;
+
+    asm ("mov %1, %0\n\t"
+        "add $1, %0"
+        : "=r" (dst)
+        : "r" (src));
+
+    // printf("%d\n", dst);
+
+    char* c = (char*)0x70;
+    kputs(c);
+
+    kputs("Exception!!");
+    while (true) {
+        __asm__ volatile ("cli; hlt");
+    }
 }
 
